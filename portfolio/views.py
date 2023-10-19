b433629d2e9774bddca55ed14a431e8f7fb964a2
LuisPra27 <e0957226681@live.uleam.edu.ec>
2023-10-19 18:57:46 -0500

    Django-Portfolio
    Paso1

---
+++ portfolio/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from .models import Project
+def home(request):
+    project = Project.objects.all()
+    return render(request, 'home.html',{'projects':project})
